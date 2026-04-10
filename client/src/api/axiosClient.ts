8dbe1e488513ee7dbd197ccb952fd94f035490bd
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-04-10 20:23:01 +0700

    push

--- client/src/api/axiosClient.ts
+++ client/src/api/axiosClient.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// Xử lý tự động chuẩn hóa URL, ngăn lỗi điền thiếu/thừa gạch chéo
 const rawDomain = import.meta.env.VITE_API_URL || "";
-// Đảm bảo baseURL lúc nào cũng auto có đít /api trước khi gọi
 const baseURL = `${rawDomain.replace(/\/$/, "")}/api`;
 
 const axiosClient = axios.create({
